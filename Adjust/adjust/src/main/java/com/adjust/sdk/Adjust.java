0ca4a8a44d77675401e6fdeadbd11c8dc410a403
uerceg <ugljesa@adjust.com>
2016-08-23 16:15:28 +0200

    New Xamarin version 4.8.0

--- Adjust/adjust/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/Adjust.java
@@ -32,7 +32,7 @@ public class Adjust {
     }
 
     public static void onCreate(AdjustConfig adjustConfig) {
-        adjustConfig.setSdkPrefix("xamarin4.0.2");
+        adjustConfig.setSdkPrefix("xamarin4.8.0");
         AdjustInstance adjustInstance = Adjust.getDefaultInstance();
         adjustInstance.onCreate(adjustConfig);
     }
