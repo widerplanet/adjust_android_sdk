e6b9bef76dca054fecd6321719f39948564e3efd
uerceg <ugljesa@adjust.com>
2016-09-27 15:21:24 +0200

    Cleanup

--- Adjust/adjust/src/main/java/com/adjust/sdk/AdjustExtension.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/AdjustExtension.java
@@ -6,18 +6,19 @@ import com.adobe.fre.FREExtension;
  * Created by pfms on 31/07/14.
  */
 public class AdjustExtension implements FREExtension{
-  public static AdjustContext context;
-  public static String LogTag = "Adjust.air";
-  @Override
-  public void initialize() { }
+    public static AdjustContext context;
+    public static String LogTag = "Adjust.air";
+  
+    @Override
+    public void initialize() {}
 
-  @Override
-  public FREContext createContext(String s) {
-    return context = new AdjustContext();
-  }
+    @Override
+    public FREContext createContext(String s) {
+        return context = new AdjustContext();
+    }
 
-  @Override
-  public void dispose() {
-    context = null;
-  }
+    @Override
+    public void dispose() {
+        context = null;
+    }
 }
