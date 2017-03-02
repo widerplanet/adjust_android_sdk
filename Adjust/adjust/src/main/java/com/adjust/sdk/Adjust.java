54b3a28cb446613d2541a2a3e2a22bc5d30fb9fc
uerceg <ugi@adjust.com>
2017-03-02 19:49:51 +0100

    Xamarin SDK prefix to 4.11.1

--- Adjust/adjust/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/Adjust.java
@@ -32,7 +32,7 @@ public class Adjust {
     }
 
     public static void onCreate(AdjustConfig adjustConfig) {
-        adjustConfig.setSdkPrefix("xamarin4.11.0");
+        adjustConfig.setSdkPrefix("xamarin4.11.1");
         AdjustInstance adjustInstance = Adjust.getDefaultInstance();
         adjustInstance.onCreate(adjustConfig);
     }
